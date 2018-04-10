c4edecbded6eb27147aec39300716e43a6bbbf22
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-04-10 15:42:52 +1200

    Simplify nix files

--- default.nix
+++ default.nix
@@ -2,31 +2,17 @@ let
   reflex-platform = import ((import <nixpkgs> {}).pkgs.fetchFromGitHub {
       owner = "reflex-frp";
       repo = "reflex-platform";
-      rev = "55bddde319d7c8f6edcc80941926c611098f0875";
-      sha256 = "0680vapmfwbj308mcg4fgr60zjb7j6in2p3lppzsd7ydlfhnsbvv";
+      rev = "f003577699ad5a47f8275dad4f05cdb15c4bcdf5";
+      sha256 = "1fwg9cfz6p6zrlk1j5648r9hc5s2m62cwwv036sc7byb3pdhlxdr";
     }) {};
+  nixpkgs = reflex-platform.nixpkgs;
   servant-auth-github = nixpkgs.fetchFromGitHub {
     owner = "hamishmack";
     repo = "servant-auth";
     rev = "77f246501cb5e83074e96a0ce419b58974406173";
     sha256 = "0g1ig082bxa7lh0yh30rbww23cmvww0al44ld3gckn0lidn3ksa2";
   };
-  nixpkgs = reflex-platform.nixpkgs;
-  # Work around bug in slightly old nixpkgs.writeShellScriptBin used by reflex-platform
-  writeShellScriptBin = name : text :
-    nixpkgs.writeTextFile {
-      inherit name;
-      executable = true;
-      destination = "/bin/${name}";
-      text = ''
-        #!${nixpkgs.stdenv.shell}
-        ${text}
-        '';
-      checkPhase = ''
-        ${nixpkgs.stdenv.shell} -n $out/bin/${name}
-      '';
-    };
-  project = reflex-platform.project ({ pkgs, ... }: {
+in reflex-platform.project ({ pkgs, ... }: {
       packages = {
         bailiwick = ./.;
         reflex-dom-contrib = pkgs.fetchFromGitHub {
@@ -46,17 +32,10 @@ let
 
       overrides = self: super: {
         servant-reflex = pkgs.haskell.lib.doJailbreak super.servant-reflex;
-        reflex-dom = null; # Work around issue where servant-reflex added webkitgtk dependency to server executable
       };
 
       shells = {
         ghc = ["bailiwick"];
         ghcjs = ["bailiwick"];
       };
-  });
-
-  shells = project.shells;
-
-in project // {
-  inherit shells writeShellScriptBin;
-}
+  })
