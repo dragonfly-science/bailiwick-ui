fde4d5366365a4f3a9195ae633a27c3d392f0307
Finlay Thompson <finlay@dragonfly.co.nz>
2018-05-17 17:54:11 +1200

    Added map with wards in it, and selectors

--- src/Bailiwick/View/Map.hs
+++ src/Bailiwick/View/Map.hs
@@ -7,9 +7,10 @@
 module Bailiwick.View.Map
 where
 
+import Debug.Trace
 
 import Control.Monad.Trans.Maybe (MaybeT(..), runMaybeT)
-import Control.Monad (mzero)
+import Control.Monad (join)
 import Control.Applicative ((<|>))
 import Control.Monad.Fix
 import Data.Monoid ((<>))
@@ -59,13 +60,21 @@ nzmap state = mdo
             Summary (r:_) -> return $ areaId r
             _             -> "new-zealand"
 
+      get field = join . fmap (fmap slugify . field)
       attrD = do 
         reg <- regD
         zoom <- zoomed
         let zoomregclass = if zoom then Just (reg <> "-zoom") else Nothing
         let zoomclass = if zoom then Just "zoom" else Nothing
-        mouse_over_reg <- (fmap slugify) <$> mouseOverRegD
-        let classes = nub $ catMaybes [ Just "map", Just reg, mouse_over_reg
+
+        mouse_over_reg <- get areaRegion <$> mouseOverD
+        mouse_over_ta  <- get areaTa <$> mouseOverD
+        mouse_over_wrd <- get areaWard <$> mouseOverD
+
+        let classes = nub $ catMaybes [ Just "map", Just reg
+                                      , mouse_over_reg
+                                      , mouse_over_ta
+                                      , mouse_over_wrd
                                       , zoomregclass, zoomclass] 
         return $ "class" =: (Text.intercalate " " classes)
 
@@ -78,24 +87,38 @@ nzmap state = mdo
   performEvent_ $ fforMaybe svgE $ (fmap $ DOM.setInnerHTML (_element_raw mapContainer))
 
   let divElement = DOM.uncheckedCastTo DOM.HTMLElement (_element_raw mapContainer)
-  moveE :: Event t (Maybe Text) 
-    <- wrapDomEvent divElement (`DOM.on` DOM.mouseMove) getRegionFromSvg
+  moveE :: Event t (Maybe AreaInfo)
+     <- wrapDomEvent divElement (`DOM.on` DOM.mouseMove) getAreaInfoFromSvg
+  clickE :: Event t (Maybe AreaInfo)
+     <- wrapDomEvent divElement (`DOM.on` DOM.click) getAreaInfoFromSvg
 
-  clickE :: Event t (Maybe Text) 
-    <- wrapDomEvent divElement (`DOM.on` DOM.click) getRegionFromSvg
+  mouseOverD :: Dynamic t (Maybe AreaInfo) <- holdDyn Nothing moveE
 
-  mouseOverRegD <- holdDyn Nothing moveE
-  return $ fmapCheap (SetRegion . maybe "new-zealand" slugify) clickE
+  -- Make the click event correctly depending on state
+  return $ fmapCheap (SetRegion . maybe "new-zealand" slugify . get areaRegion) clickE
 
+data AreaInfo
+  = AreaInfo
+    { areaRegion :: Maybe Text
+    , areaTa     :: Maybe Text
+    , areaWard   :: Maybe Text
+    }
 
-getRegionFromSvg :: DOM.IsEvent ev => DOM.EventM e ev (Maybe Text)
-getRegionFromSvg = runMaybeT $ do
+getAreaInfoFromSvg
+    :: DOM.IsEvent ev 
+    => DOM.EventM e ev (Maybe AreaInfo)
+getAreaInfoFromSvg = runMaybeT $ do
   target     <- MaybeT DOM.eventTarget
   svgelement <- MaybeT (DOM.castTo DOM.SVGElement target)
   parentg    <- MaybeT (DOM.getParentElement svgelement)
-  let getAttr a = do
+  let getAttr :: Text -> MaybeT (DOM.EventM e ev) (Maybe Text)
+      getAttr a = do
          val <- MaybeT (DOM.getAttribute parentg a)
          if val == "null"
-            then mzero
-            else return val
-  getAttr ("reg" :: Text) <|> getAttr "reg1"
+            then return Nothing
+            else return (Just val)
+  reg <- getAttr "reg" <|> getAttr "reg1"
+  ta  <- getAttr "ta"  <|> getAttr "ta1"
+  wrd <- getAttr "wrd" <|> getAttr "wrd1"
+  return $ AreaInfo reg ta wrd
+
