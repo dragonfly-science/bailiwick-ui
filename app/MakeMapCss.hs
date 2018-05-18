9ad69817967a4f483c173f5721fe8327e01b9ec6
Finlay Thompson <finlay@dragonfly.co.nz>
2018-05-19 02:58:22 +1200

    Made the css selectors work

--- app/MakeMapCss.hs
+++ app/MakeMapCss.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 {- Script to make the map.css file, based on the area data -}
 
 import System.Environment (getArgs)
-import Data.Monoid ((<>))
+import Data.Maybe (listToMaybe)
 import Data.Aeson (eitherDecode)
 import qualified Data.ByteString.Lazy as BS
+import Data.Text(Text, unpack)
+import qualified Data.Text as T
+
 
 import Bailiwick.Types
 
@@ -20,12 +25,24 @@ main = do
             _     -> "static/data/areas-1b7549470.json"
   
   -- Read in the data json file
-  areas :: [Area] <- readAreas jsonfile
+  areas <- readAreas jsonfile
 
   -- Produce a list of CSS selectors for ta's when zoomed
-  let selectors :: [ BS.ByteString ]
-      selectors = [
-  div.canterbury.canterbury-zoom.map g.area[reg="Canterbury"] > path
+  putStrLn $ unpack $ T.unlines $ 
+      format "div.map.zoom.region.ta g.area[reg=\"taName\"] > path"
+          <$> filter (\a -> areaLevel a == "ta") areas
+
+-- div.canterbury.canterbury-zoom.map g.area[reg="Canterbury"] > path
+
+format ::  Text -> Area -> Text
+format = flip format'
+  where
+    format' Area{..}
+       = T.replace "ta" areaId
+       . T.replace "region" ( maybe "" id $
+              listToMaybe [ p | p <- areaParents 
+                              , p /= "new-zealand" ])
+       . T.replace "taName" areaName
 
 
 
