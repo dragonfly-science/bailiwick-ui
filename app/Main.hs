5c306f80f7d4cf482a1369be1d96bec78952ba77
Finlay Thompson <finlay@dragonfly.co.nz>
2019-03-03 23:05:51 +1300

    Big tidyup

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ module Main (
 ) where
 
 #ifdef MIN_VERSION_jsaddle_wkwebview
+import Data.Default(Default(..))
+import qualified Data.ByteString.Lazy as LBS (toStrict)
 import Language.Javascript.JSaddle.WKWebView (run, runFile, runHTMLWithBaseURL)
 #else
 import Language.Javascript.JSaddle.Warp (run)
@@ -12,19 +14,9 @@ import Language.Javascript.JSaddle.Warp (run)
 import Reflex.Dom.Core (mainWidget)
 
 import Bailiwick (ui)
-import Data.Default (Default(..))
-import qualified Data.ByteString as BS (pack)
-import System.Directory (getCurrentDirectory)
-import qualified Data.Text.Encoding as T (encodeUtf8)
-import qualified Data.Text as T (pack)
-import Data.Monoid ((<>))
-import Bailiwick.Application (indexHtml)
-import qualified Data.ByteString.Lazy as LBS (toStrict)
 
 main :: IO ()
 main = do
-  dir <- getCurrentDirectory
-  html <- indexHtml []
 #ifdef MIN_VERSION_jsaddle_wkwebview
   runHTMLWithBaseURL (LBS.toStrict html) "http://localhost:3701/" def $
 #else
