0c298d73cf0e8453d85e9ff4355689282bc70457
Finlay Thompson <finlay@dragonfly.co.nz>
2019-04-04 17:50:51 +1300

    Getting the bailwick.js to the frontend

--- deploy.nix
+++ deploy.nix
@@ -9,6 +9,7 @@ let
     cp -r ${./static}/* $out/static
     chmod +w $out/static
     ${nixpkgs.closurecompiler}/bin/closure-compiler ${ghcjs.bailiwick}/bin/bailiwick.jsexe/all.js > $out/static/min.js
+    ${nixpkgs.closurecompiler}/bin/closure-compiler $out/static/bailiwick.js > $out/static/bailiwick.min.js
   '';
 in bailiwick-static
 
