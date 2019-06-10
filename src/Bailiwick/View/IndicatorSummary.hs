cd038c08565229a3f2c11bfba1345122d5d4d96c
Simon Winter <simon@saltedherring.com>
2019-06-10 14:41:02 +1200

    Added html for table view - requires reflex code to wire up #15 #8

--- src/Bailiwick/View/IndicatorSummary.hs
+++ src/Bailiwick/View/IndicatorSummary.hs
@@ -88,6 +88,8 @@ indicatorSummary IndicatorSummaryState{..} = mdo
           year <- myear
           OM.lookup (areaid, year, Nothing) ismap
 
+  showTableD <- holdDyn False $ not <$> tag (current showTableD) showTableE
+
   divClass "summary" $
     divClass "intersection" $ do
       divClass "intersection-number headline-number" $ do
@@ -113,16 +115,91 @@ indicatorSummary IndicatorSummaryState{..} = mdo
         text "Notes:"
         void . dyn $ ffor (fromMaybe [] . (indicatorNotes =<<) <$> indicatorD)
                        (mapM_ $ elDynHtmlAttr' "p" (constDyn mempty) . subs . constDyn)
+  
   elDynClass "div" (("table-view " <>) . bool "hide" "show" <$> showTableD) $
     elAttr "div" ("class" =: "panel" <> "style" =: "height: 799px;") $ do
-      el "header" $
+      el "header" $ do
         divClass "table-caption text" $
           text "TODO"
+        divClass "controls" $ do
+          -- requires an event to close the table.
+          elAttr "button" ("class" =: "close") $
+            el "i" $ return ()
+     
       elAttr "div" ("class" =: "table-container" <> "style" =: "height: 657px;") $
-        el "div" $
-          text "TODO"
+        -- ##
+        -- There are 2 tables - 1 with a single region/indicator,
+        -- the second with comparison data.
+        -- ##
 
-  showTableD <- holdDyn False $ not <$> tag (current showTableD) showTableE
+        divClass "single-table" $ do
+          -- may need logic to determine when to use "show" on the table class.
+          elAttr "table" ("class" =: "table-sorter show") $ do
+            el "tfoot" $
+              el "tr" $
+                elAttr "td" ("class" =: "button" <> "colspan" =: "4") $ do
+                  -- there needs to be logic to check whether there can be
+                  -- an export button
+                  elAttr "button" ("class" =: "export") $ text "Export CSV"
+            el "thead" $
+              el "tr" $ do
+                -- Each th element needs an event to add "tablesorter-headerDesc" or
+                -- "tablesorter-headerAsc" if the column is being sorted.
+                elAttr "th" ("class" =: "year tablesorter-headerDesc") $ text "Year"
+                -- The next 3 header rows need to have correctly formatted titles
+                -- - e.g. "The estimated resident population", "The annual
+                -- percentage chage in ..."
+                elAttr "th" ("class" =: "original") $ text "Original"
+                elAttr "th" ("class" =: "transform") $ text "Transform"
+                elAttr "th" ("class" =: "national") $ text "National"
+            el "tbody" $ 
+              -- each row represents 1 row in the data set
+              el "tr" $ do
+                el "td" $ text "Row Year"
+                elAttr "td" ("class" =: "colour-teal") $ text "Row Original"
+                elAttr "td" ("class" =: "colour-lighter-blue") $ text "Row Transform"
+                elAttr "td" ("class" =: "colour-green") $ text "Row National"
+
+        -- ##
+        -- If the comparision option is enabled, we show the comparison data...
+        -- ##
+        -- divClass "compare-table" $ do
+        --   -- may need logic to determine when to use "show" on the table class.
+        --   elAttr "table" ("class" =: "table-sorter show") $ do
+        --     el "tfoot" $
+        --       el "tr" $ do
+        --         elAttr "td" ("class" =: "button") $ do
+        --           -- there needs to be logic to check whether there can be
+        --           -- an export button
+        --           elAttr "button" ("class" =: "export") $ text "Export CSV"
+        --         elAttr "td" ("colspan" =: "3") $ do
+        --           el "span" $ text "The estimated resident population -- original label"
+        --         elAttr "td" ("colspan" =: "2") $ do
+        --           el "span" $ text "The annual percentage change in the estimated resident population -- transform label"
+        --     el "thead" $
+        --       el "tr" $ do
+        --         -- Each th element needs an event to add "tablesorter-headerDesc" or
+        --         -- "tablesorter-headerAsc" if the column is being sorted.
+        --         elAttr "th" ("class" =: "double tablesorter-headerDesc") $ text "Year"
+        --         -- The next header rows need to have correctly formatted titles
+        --         -- - e.g. "The estimated resident population", "The annual
+        --         -- percentage chage in ..."
+        --         elAttr "th" ("class" =: "border-top area") $ text "Area Name"
+        --         elAttr "th" ("class" =: "border-top compare") $ text "Compare Area Name"
+        --         elAttr "th" ("class" =: "border-top double ratio") $ text "Ratio"
+        --         elAttr "th" ("class" =: "border-top area-t") $ text "Area Name"
+        --         elAttr "th" ("class" =: "border-top compare-t") $ text "Compare Area Name"
+        --     el "tbody" $ 
+        --       -- each row represents 1 row in the data set
+        --       el "tr" $ do
+        --         elAttr "td" ("class" =: "double") $ text "Row Year"
+        --         elAttr "td" ("class" =: "colour-green") $ text "Row Area"
+        --         elAttr "td" ("class" =: "colour-compare-green") $ text "Row Compare Area"
+        --         elAttr "td" ("class" =: "double") $ text "Row Ratio"
+        --         elAttr "td" ("class" =: "colour-green") $ text "Row Area-t"
+        --         elAttr "td" ("class" =: "colour-compare-green") $ text "Row Compare-t"
+
+  
   showTableE <- fmap (domEvent Click . fst) $
     divClass "table-button" $
       elClass' "button" "show-table" $
