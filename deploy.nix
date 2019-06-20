d32b6b0fc2b1806e843e2c5cd61a3399e1252043
Finlay Thompson <finlay@dragonfly.co.nz>
2019-06-20 13:35:14 +1200

    remove --externs=node

--- deploy.nix
+++ deploy.nix
@@ -8,7 +8,7 @@ let
     mkdir -p $out/static
     cp -r ${./static}/* $out/static
     chmod +w $out/static
-    ${nixpkgs.closurecompiler}/bin/closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS --jscomp_off=checkVars --externs=node --externs=all.js.externs ${ghcjs.bailiwick}/bin/bailiwick.jsexe/all.js   $out/static/bailiwick.bundle.js  > $out/static/min.js
+    ${nixpkgs.closurecompiler}/bin/closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS --jscomp_off=checkVars --externs=all.js.externs ${ghcjs.bailiwick}/bin/bailiwick.jsexe/all.js   $out/static/bailiwick.bundle.js  > $out/static/min.js
   '';
 in bailiwick-static
 
