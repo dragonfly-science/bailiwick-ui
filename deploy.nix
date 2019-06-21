49a73a7245265242d352f2712fab9c36b36af293
Finlay Thompson <finlay@dragonfly.co.nz>
2019-06-21 15:50:33 +1200

    Added ADVANCE_OPTIMIZATIONS

--- deploy.nix
+++ deploy.nix
@@ -9,6 +9,7 @@ let
     cp -r ${./static}/* $out/static
     chmod +w $out/static
     ${nixpkgs.closurecompiler}/bin/closure-compiler \
+        --compilation_level ADVANCED_OPTIMIZATIONS \
         --jscomp_off=checkVars \
         --externs=${ghcjs.bailiwick}/bin/bailiwick.jsexe/all.js.externs \
         ${ghcjs.bailiwick}/bin/bailiwick.jsexe/all.js   > $out/static/min.js
