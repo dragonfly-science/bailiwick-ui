ff4461b83eb55f409e602195e640e182cc750136
Finlay Thompson <finlay@dragonfly.co.nz>
2019-09-09 13:40:51 +1200

    Diagnosing build problem

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -ex
 
 ## Make the d3, non-reflex javascript
 nix-build javascript.nix -A package
+ls -lR result
 cp result/lib/node_modules/bailiwick-ui/static/bailiwick.bundle.js static/
 
 ## make the javascript executible
