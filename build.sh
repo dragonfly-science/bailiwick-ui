9da1c3e5b72aa2fba3b625c5c501ee567c34b550
Finlay Thompson <finlay@dragonfly.co.nz>
2019-09-09 14:23:53 +1200

    Updated node2nix settings

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -ex
 
 ## Make the d3, non-reflex javascript
 nix-build javascript.nix -A package
-ls -lR result/
-cp result/lib/node_modules/bailiwick-ui/static/bailiwick.bundle.js static/
+cp result/bin/bailiwick.bundle.js static/
 
 ## make the javascript executible
 nix-build deploy.nix
